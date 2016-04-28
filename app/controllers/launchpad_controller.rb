946adcca82927bf1dc1b954a223b4cd00da5382c
Tludi <tludi00@gmail.com>
2016-04-28 12:53:48 -0700

    converted landing page to launchpad page. Added launchpad js files needed

--- app/controllers/launchpad_controller.rb
+++ app/controllers/launchpad_controller.rb
@@ -2,6 +2,7 @@ class LaunchpadController < ApplicationController
   def index
     @mainTitle = "Welcome to the launchpad"
     @mainDesc = "time to launch!"
+    render :layout => "empty"
 
   end
 end
