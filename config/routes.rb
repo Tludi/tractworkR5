3b3edd5e72232bb8987c91e8247be6b9a577ce1d
Tludi <tludi00@gmail.com>
2016-04-28 15:44:33 -0700

    added basic iboxes to dashboard index. added favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Myapp::Application.routes.draw do
-  get 'dashboard/index'
-
   get 'launchpad/index'
 
-  get 'home' => 'home#index', :as => :home
+  get 'dashboard' => 'dashboard#index', :as => :dashboard
   # get "home/index"
   get "home/minor"
 
