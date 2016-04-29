4e10fa51c454e50fd0220b6269913cf4df650bb1
Tludi <tludi00@gmail.com>
2016-04-28 17:38:02 -0700

    added user sessions and integrated authentication into application

--- app/controllers/launchpad_controller.rb
+++ app/controllers/launchpad_controller.rb
@@ -1,4 +1,5 @@
 class LaunchpadController < ApplicationController
+  skip_before_action :require_login
   def index
     #any page variable need to be set before rendering layout
     render :layout => "launchpad"
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Myapp::Application.routes.draw do
+  # get 'user_sessions/new'
+  # get 'user_sessions/create'
+  # get 'user_sessions/destroy'
+  resources :user_sessions
+  resources :users
+  # get 'password_resets/create'
+  # get 'password_resets/edit'
+  # get 'password_resets/update'
+  resources :password_resets
+  get 'login' => 'user_sessions#new', as: :login
+  get 'logout' => 'user_sessions#destroy', as: :logout
   get 'launchpad/index'
 
   get 'dashboard' => 'dashboard#index', :as => :dashboard
